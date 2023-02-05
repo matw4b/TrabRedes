285bd6879fa89f70dedad9b9358b8c81c0b7dbeb
LeonardoMaximoSilva <leomax2002@gmail.com>
2023-02-05 18:56:05 -0300

    V5

--- r2anewalgoritm1.py
+++ r2anewalgoritm1.py
@@ -40,48 +40,43 @@ class R2ANewAlgoritm1(IR2A):
 
         self.request_time = time.perf_counter()
         n = self.n
-        #print("Índice: ", self.indice)
+
         if self.throughputs == []: #se o algoritmo acabou de comecar, entao a qualidade escolhida sera a pior
             qualidade = self.qi[0]
             self.indice = 0
-        else: #senao
+
+        else: #se nao
             qualidade = self.qi[self.indice] #coloca a qualidade anterior na variavel
             media = 0
+
             if(len(self.throughputs) < n):
                 media = mean(self.throughputs)
+
             else:
                 for i in range(n):
                     media += self.throughputs[len(self.throughputs)-1-i]/n  # media das taxas
-            mad_weighted = 0 #media do desvio absoluto com pesos (pesquise apenas mad no google).mad mostra se a estabilidade da rede
-            if(len(self.throughputs) >= n):
-                for i in range(10):
-                    mad_weighted += ((n - i)/(n))*(abs(self.throughputs[len(self.throughputs)-1-i] - media)) #o vetor ja esta ordenado do dado mais antigo para o dado mais recente
-                #colocamos um peso nos itens da taxa para que os mais recentes tenham peso maior
-            #mad_weighted = (mad_weighted)/len(self.throughputs)
+
+            mad_weighted = 0 #media do desvio absoluto com pesos (pesquise apenas mad no google).mad mostra a estabilidade da rede
+            
+            if(len(self.throughputs) >= n): #o vetor ja esta ordenado do dado mais antigo para o dado mais recente
+                for i in range(n):
+                    mad_weighted += ((n - i)/(n))*(abs(self.throughputs[len(self.throughputs)-1-i] - media)) 
+                    #colocamos um peso nos itens da taxa para que os mais recentes tenham peso maior
             else:
                 ind = 0
                 for item in self.throughputs:
                     mad_weighted += ((ind + 1)/(len(self.throughputs)))*(abs(item - media)) #o vetor ja esta ordenado do dado mais antigo para o dado mais recente
                     ind+=1
+                    #colocamos um peso nos itens da taxa para que os mais recentes tenham peso maior
+
             probabilidade = (media)/(media + mad_weighted) #tendencia de mudar de qualidade
             aumentar = probabilidade*(self.qi[min(len(self.qi), self.indice)]) #tendencia de aumentar a qualidade
             diminuir = (1-probabilidade)*(self.qi[max(0, self.indice-1)]) #tendencia de diminuir a qualidade
-            print("Qualidade Real: " , qualidade)
             qualidadeAux = qualidade - diminuir + aumentar #atualiza o valor da qualidade
-            print("Média: " , media)
-            print("DesvPad: " , mad_weighted)
-            #print("Probabilidade: " , probabilidade)
-            print("aumentar: " , aumentar)
-            print("Diminuir: " , diminuir)
-            print("Qualidade: " , qualidadeAux)
-            #print(self.throughputs)
-            print(self.qi)
+
             aux = 0
-            for item in self.qi:
-                #print("Qualidade atual",item)
-                #print("Indice", aux)
-                if qualidadeAux > item:
-                    #print("Qualidade posível",item)
+            for item in self.qi: #Define o proximo QI escolhido (aux) com base na Qualidade Calculada (qualidadeAux)
+                if qualidadeAux > item: #Escolhe-se sempre a menor Qualidade dentre as maiores qualidades possiveis
                     qualidade = item
                     if aux < (len(self.qi)-1):
                         aux+=1
@@ -101,5 +96,3 @@ class R2ANewAlgoritm1(IR2A):
 
     def finalization(self):
         pass
-
-
